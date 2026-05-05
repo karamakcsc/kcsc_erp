db08417a4d5914358b71ef8ca9340d5290b381d7
karamakcsc <karamakcsc@hotmail.com>
2026-05-05 18:39:54 +0800

    05-05-2026

--- apps/masar_haus/masar_haus/hooks.py
+++ apps/masar_haus/masar_haus/hooks.py
@@ -246,6 +246,7 @@ app_license = "mit"
 # ------------
 # List of apps whose translatable strings should be excluded from this app's translations.
 # ignore_translatable_strings_from = []
+###
 
 fixtures = [
     {"dt": "Custom Field", "filters": [
